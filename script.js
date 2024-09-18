ec7085c00a90b3875f6483f99215b3a66409d1c4
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-18 15:36:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //complete this code
+
+
 class Animal {
 	constructor(species){
 		this._species = species;
@@ -13,14 +15,14 @@ class Animal {
 }
 
 class Dog extends Animal {
-	purr(){
-		console.log("purr");
+	bark(){
+		console.log("woof");
 	}
 }
 
 class Cat extends Animal {
-	bark(){
-		console.log("woof");
+	purr(){
+		console.log("purr");
 	}
 }
 
